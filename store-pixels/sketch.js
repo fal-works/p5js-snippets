34e52724a4a886583cf0eb7e3ff3c04e9aa5c09f
FAL <falworks.contact@gmail.com>
2019-11-27 01:43:57 +0900

    add "store-pixels"

---
+++ store-pixels/sketch.js
@@ -0,0 +1,50 @@
+// ---- definition ------------------------------------------------------------
+
+/**
+ * Stores the canvas pixels and returns a function that restores them.
+ * @return A function that restores the canvas pixels.
+ */
+const storePixels = () => {
+  loadPixels();
+  const storedPixels = pixels;
+
+  return () => {
+    pixels = storedPixels;
+    updatePixels();
+  };
+};
+
+// ---- example ---------------------------------------------------------------
+
+// -- functions ----
+
+// -- main ----
+
+let restorePixels;
+
+function setup() {
+  createCanvas(640, 480);
+
+  rectMode(CENTER);
+
+  background(255);
+  noStroke();
+  fill(32, 0, 255, 16);
+
+  for (let i = 0; i < 100; i += 1) {
+    rect(random(width), random(height), 100, 20);
+    rect(random(width), random(height), 20, 100);
+  }
+
+  restorePixels = storePixels();
+
+  stroke(0, 160);
+  noFill();
+  strokeWeight(10);
+}
+
+function draw() {
+  restorePixels();
+
+  square(width / 2, height / 2, 300 + 100 * Math.sin(frameCount * 0.1));
+}
